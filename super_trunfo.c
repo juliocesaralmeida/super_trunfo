64430691cd0329cc8dad7c15a6d0bc113e89af9d
Julio Cesar <109310249+juliocesaralmeida@users.noreply.github.com>
2025-03-24 21:50:23 -0300

    Update super_trunfo.c
    adicionado novas funcionalidades de comparação entre as duas cartas e mostrando a vencedora

--- super_trunfo.c
+++ super_trunfo.c
@@ -4,12 +4,13 @@ int main (){
     char estado1, estado2;
     char codigo1[5], codigo2[5];
     char nomecidade1[100], nomecidade2[100];
-    int populacao1, populacao2;
+    unsigned long int populacao1, populacao2;
     float area1, area2;
     float pib1, pib2;
     int pontosturisticos1, pontosturisticos2;
     float densidade1, densidade2;
     float pibpercapita1, pibpercapita2;
+    float superpoder1, superpoder2;
 
     //primeira carta 
     printf("Carta 1:\n");
@@ -17,13 +18,13 @@ int main (){
     scanf(" %c", &estado1); //lê a inicial do estado
 
     printf("Digite o código da carta(ex:A01, B03): \n");
-    scanf("%s", &codigo1); //lê o código da carta 
+    scanf("%s", codigo1); //lê o código da carta 
 
     printf("Digite o nome da cidade: \n");
-    scanf("%s", &nomecidade1); //lê o nome da cidade 
+    scanf("%s", nomecidade1); //lê o nome da cidade 
 
     printf("Digite a população atual: \n");
-    scanf("%d", &populacao1);// lê a população
+    scanf("%lu", &populacao1);// lê a população
 
     printf("Digite a área (em km²): \n");
     scanf("%f", &area1); //lê a área
@@ -36,6 +37,7 @@ int main (){
 
     densidade1 = populacao1 / area1;
     pibpercapita1 = pib1 / populacao1;
+    superpoder1 = populacao1 + area1 + pib1 + pontosturisticos1 + pibpercapita1 + (1.0 / densidade1);
 
     //segunda carta
     printf("Carta 2:\n");
@@ -43,13 +45,13 @@ int main (){
     scanf(" %c", &estado2);
 
     printf("Digite o código da carta(ex:A01, B03): \n");
-    scanf("%s", &codigo2);
+    scanf("%s", codigo2);
 
     printf("Digite o nome da cidade: \n");
-    scanf("%s", &nomecidade2);
+    scanf("%s", nomecidade2);
     
     printf("Digite a população atual: \n");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("Digite a área (em km²): \n");
     scanf("%f", &area2);
@@ -62,13 +64,14 @@ int main (){
 
     densidade2 = populacao2 / area2;
     pibpercapita2 = pib2 / populacao2;
+    superpoder2 = populacao2 + area2 + pib2 + pontosturisticos2 + pibpercapita2 + (1.0 / densidade2);
     
     //exibindo a carta 1
     printf("Carta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
     printf("Nome da cidade: %s\n", nomecidade1);
-    printf("População: %d\n", populacao1);
+    printf("População: %lu\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f milhões de reais\n", pib1);
     printf("Pontos Turísticos: %d\n", pontosturisticos1);
@@ -80,14 +83,29 @@ int main (){
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da cidade: %s\n", nomecidade2);
-    printf("População: %d\n", populacao2);
+    printf("População: %lu\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f milhões de reais\n", pib2);
     printf("Pontos Turísticos: %d\n", pontosturisticos2);
     printf("Densidade Populacional: %.2f\n", densidade2);
     printf("PIB per capita: %.2f\n", pibpercapita2);
-    
 
-    return 0;
+    // comparando atributo por atributo
+
+    printf("População: %s\n", (populacao1 > populacao2) ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+
+    printf("Área: %s\n", (area1 > area2) ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+
+    printf("PIB: %s\n", (pib1 > pib2) ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+
+    printf("Pontos turísticos: %s\n", (pontosturisticos1 > pontosturisticos2) ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+
+    printf("PIB per capita: %s\n", (pibpercapita1 > pibpercapita2) ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+
+    printf("Densidade Populacional: %s\n", (densidade1 < densidade2) ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+
+    printf("Super Poder: %s\n", (superpoder1 > superpoder2) ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+
+
 
 }
